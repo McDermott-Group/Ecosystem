a28b033c617aff3edd3ad00f08566dde671c9a00
Alexander Opremcak <opremcak@wisc.edu>
2015-12-07 20:37:08 -0600

    Removed miscellaneous servers

--- QubitServer/src/main/scala/org/labrad/qubits/channels/FiberChannel.scala
+++
@@ -1,8 +0,0 @@
-package org.labrad.qubits.channels
-
-import org.labrad.qubits.enums.DcRackFiberId
-
-trait FiberChannel extends Channel {
-  def getDcFiberId(): DcRackFiberId
-  def setBiasChannel(channel: DcRackFiberId): Unit
-}
