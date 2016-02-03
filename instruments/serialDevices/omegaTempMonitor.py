f564f5bcaad5cdb5490aa3aa54b3b4ba6fb6fe2e
Alex Opremcak <opremcak@wisc.edu>
2016-02-03 15:50:45 -0600

    Adding serial template for Omega Temperature Monitor that needs to be finished

---
+++ instruments/serialDevices/omegaTempMonitor.py
@@ -0,0 +1,123 @@
+# Copyright (C) 2016  Noah Meltzer && Alexander Opremcak
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+### BEGIN NODE INFO
+[info]
+name = Omega Temp Monitor Server
+version = 1.00013
+description = 
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+[shutdown]
+message = 987654321
+timeout = 20
+### END NODE INFO
+"""
+
+from labrad.types import Value
+from labrad.devices import DeviceServer, DeviceWrapper
+from labrad.server import LabradServer, setting
+from labrad.errors import Error
+import labrad.units as units
+from twisted.internet.defer import inlineCallbacks, returnValue
+from labrad import util
+
+    
+class OmegaTempMonitorWrapper(DeviceWrapper):
+    
+    @inlineCallbacks
+    def connect(self, server, port):
+        """Connect to a cold switch board."""
+        print 'connecting to "%s" on port "%s"...' % (server.name, port),
+        self.server = server
+        self.ctx = server.context()
+        self.port = port
+        p = self.packet()
+        p.open(port)
+        p.baudrate(9600L)
+        p.stopbits(1L)
+        p.bytesize(7L)
+        p.parity('E')
+        p.read() # clear out the read buffer
+        p.timeout(TIMEOUT)
+        yield p.send()
+        print 'done.'
+        
+    def packet(self):
+        """Create a packet in our private context."""
+        return self.server.packet(context=self.ctx)
+    
+    def shutdown(self):
+        """Disconnect from the serial port when we shut down."""
+        return self.packet().close().send()
+    
+    @inlineCallbacks
+    def write(self, code, index = 0):
+        """Write a data value to the cold switch."""
+        p = self.packet()
+        p.write(code)
+        yield p.send()
+             
+class OmegaTempMonitorServer(DeviceServer):
+    deviceName = 'Omega Temp Monitor Server'
+    name = 'Omega Temp Monitor Server'
+    deviceWrapper = OmegaTempMonitorWrapper
+    
+    @inlineCallbacks
+    def initServer(self):
+        print 'loading config info...',
+        self.reg = self.client.registry()
+        yield self.loadConfigInfo()
+        print 'done.'
+        yield DeviceServer.initServer(self)
+    
+    @inlineCallbacks
+    def loadConfigInfo(self):
+        """Load configuration information from the registry."""
+        reg = self.reg
+        yield reg.cd(['', 'Servers', 'Omega Temp Monitor', 'Links'], True)
+        dirs, keys = yield reg.dir()
+        p = reg.packet()
+        for k in keys:
+            p.get(k, key=k)
+        ans = yield p.send()
+        self.serialLinks = dict((k, ans[k]) for k in keys)
+    
+    @inlineCallbacks    
+    def findDevices(self):
+        """Find available devices from list stored in the registry."""
+        devs = []
+        print "self.serialLinks.items()=", self.serialLinks.items()
+        for name, (server, port) in self.serialLinks.items():
+            if server not in self.client.servers:
+                continue
+            server = self.client[server]
+            ports = yield server.list_serial_ports()
+            if port not in ports:
+                continue
+            devName = '{} - {}'.format(server, port)
+            devs += [(name, (server, port))]
+        print "devs=", devs
+        returnValue(devs)
+         
+TIMEOUT = 1*units.s
+
+__server__ = OmegaTempMonitorServer()
+
+if __name__ == '__main__':
+    from labrad import util
+    util.runServer(__server__)
