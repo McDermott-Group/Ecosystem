3bdbbb5a5de20c4f1b96eecf2c8304beae1a377b
amopremcak <opremcak@wisc.edu>
2016-03-24 14:34:14 -0500

    test script update

--- dataChest/dataChestTestScript.py
+++ dataChest/dataChestTestScript.py
@@ -16,55 +16,55 @@ d.addParameter("X Label", "Time")
 d.addParameter("Y Label", "Digitizer Noise")
 d.addParameter("Plot Title", "Gaussian Noise Generator")
 
-###create damped sinusoid
-##t1 = np.arange(0.0, 2.0, 0.00001)
-##d.createDataset("DampedOscillations", [("time", [len(t1)], "float64", "Seconds")],
-##                [("Oscillatory", [len(t1)], "float64", "Volts"),
-##                 ("Underdamped", [len(t1)], "float64", "Volts"),
-##                 ("Envelope", [len(t1)], "float64", "Volts")])
-##d.addParameter("X Label", "Time")
-##d.addParameter("Y Label", "Voltage")
-##d.addParameter("Plot Title", "Damped Oscillations")
-##d.addData([[t1, np.sin(2 * np.pi * t1),
-##            np.exp(-t1) * np.sin(2 * np.pi * t1),
-##            np.exp(-t1)]])
+#create damped sinusoid
+t1 = np.arange(0.0, 2.0, 0.00001)
+d.createDataset("DampedOscillations", [("time", [len(t1)], "float64", "Seconds")],
+                [("Oscillatory", [len(t1)], "float64", "Volts"),
+                 ("Underdamped", [len(t1)], "float64", "Volts"),
+                 ("Envelope", [len(t1)], "float64", "Volts")])
+d.addParameter("X Label", "Time")
+d.addParameter("Y Label", "Voltage")
+d.addParameter("Plot Title", "Damped Oscillations")
+d.addData([[t1, np.sin(2 * np.pi * t1),
+            np.exp(-t1) * np.sin(2 * np.pi * t1),
+            np.exp(-t1)]])
 
-###Arbitrary 2D datasets DO THIS TONIGHT
-##x = np.random.rand(100)
-##y = np.random.rand(100)
-##z = np.sin(x)+np.cos(y)
-##
-##d.createDataset("LogarithmicWaveform", [("indepName1", [len(x)], "float64", "Volts"),
-##                                        ("indepName2", [len(x)], "float64", "Power")],
-##                                       [("depName1", [len(x)], "float64", "Current")])
-##
-##d.addParameter("X Label", "Voltage")
-##d.addParameter("Y Label", "Power")
-##d.addParameter("Plot Title", "Q(Voltage,Power)")
-##d.addData([[x,y,z]])
+#Arbitrary 2D datasets DO THIS TONIGHT
+x = np.random.rand(100)
+y = np.random.rand(100)
+z = np.sin(x)+np.cos(y)
+
+d.createDataset("LogarithmicWaveform", [("indepName1", [len(x)], "float64", "Volts"),
+                                        ("indepName2", [len(x)], "float64", "Power")],
+                                       [("depName1", [len(x)], "float64", "Current")])
+
+d.addParameter("X Label", "Voltage")
+d.addParameter("Y Label", "Power")
+d.addParameter("Plot Title", "Q(Voltage,Power)")
+d.addData([[x,y,z]])
 
 #2D Pixel Sweeps (include log possibilities at some point)
-##t1 = np.arange(-5.0, 5.0, step=0.05)
-##t2 = np.arange(-5.0, 5.0, step=0.05)
-##ImageData = []
-##for jj in range(0, len(t2)):
-##    for ii in range(0, len(t1)):
-##        #if jj<len(t2)-1:
-##        ImageData.append([t1[ii], t2[jj], np.sin(t1[ii]**2+t2[jj]**2)])
-##
-##d.createDataset("PixelPlot", [("indepName1", [1], "float64", "Volts"),
-##                                        ("indepName2", [1], "float64", "Power")],
-##                                       [("depName1", [1], "float64", "Current")])
-##
-##d.addParameter("X Label", "Bias1")
-##d.addParameter("Y Label", "Bias2")
-##d.addParameter("Plot Title", "Q(Bias1,Bias2)")
-##d.addParameter("Image Type", "Pixel")
-##d.addParameter("X Resolution", len(t1))
-##d.addParameter("Y Resolution", len(t1))
-##d.addParameter("X Increment", 0.05)
-##d.addParameter("Y Increment", 0.05)
-##d.addData(ImageData)
+t1 = np.arange(-5.0, 5.0, step=0.05)
+t2 = np.arange(-5.0, 5.0, step=0.05)
+ImageData = []
+for jj in range(0, len(t2)):
+    for ii in range(0, len(t1)):
+        #if jj<len(t2)-1:
+        ImageData.append([t1[ii], t2[jj], np.sin(t1[ii]**2+t2[jj]**2)])
+
+d.createDataset("PixelPlot", [("indepName1", [1], "float64", "Volts"),
+                                        ("indepName2", [1], "float64", "Power")],
+                                       [("depName1", [1], "float64", "Current")])
+
+d.addParameter("X Label", "Bias1")
+d.addParameter("Y Label", "Bias2")
+d.addParameter("Plot Title", "Q(Bias1,Bias2)")
+d.addParameter("Image Type", "Pixel")
+d.addParameter("X Resolution", len(t1))
+d.addParameter("Y Resolution", len(t1))
+d.addParameter("X Increment", 0.05)
+d.addParameter("Y Increment", 0.05)
+d.addData(ImageData)
 
 
 #OLDDDDD
