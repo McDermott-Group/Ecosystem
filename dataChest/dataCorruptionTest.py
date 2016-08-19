257974923620b7b8aae5cc2ec6ba3703a9360297
Alexander Opremcak <opremcak@wisc.edu>
2016-08-19 11:46:54 -0600

    Modified dataCorruptionTest to be more intensive for Noah to test.

--- dataChest/dataCorruptionTest.py
+++ dataChest/dataCorruptionTest.py
@@ -1,19 +1,21 @@
 from dataChest import *
 import numpy as np
 import time
-d1 = dataChest(["dataCorruption2"])
+
+#create array of dataChest objects for data corruption tests
+dataChestObjArray = []
+for objIndex in range(0,100):
+    dataChestObjArray.append(dataChest(["dataCorruptionTest"]))
+    d = dataChestObjArray[objIndex]
+    d.createDataset("AllOnes", [("indepName1", [1], "float64", "s")], [("depName1", [1], "float64", "V")])
+    d.addParameter("X Label", "Time")
+    d.addParameter("Y Label", "All Ones")
+    d.addParameter("Plot Title", "Is Dataset Corrupted")
 
 
-#1D Arbitrary Data (Option 1 Style)
-#Random Number Generator
-print "1D Arbitrary Data (Option 1 Style):" #Most inneficient style by far
-numPoints = 1e6
-print "\tNumber of Points =", numPoints
-d1.createDataset("Histogram1by1", [("indepName1", [1], "float64", "Seconds")], [("depName1", [1], "float64", "Volts")])
-d1.addParameter("X Label", "Time")
-d1.addParameter("Y Label", "Digitizer Noise")
-d1.addParameter("Plot Title", "Random Number Generator")
-
-for ii in range(0, int(numPoints)):
-    d1.addData([[float(ii),1.0]])
+for ii in range(0, 12*60*60): #will take about ~13 hours since it takes finite time to write to all 100 files
+    for objIndex in range(0,100):
+        d = dataChestObjArray[objIndex]
+        d.addData([[float(ii),1.0]])
     time.sleep(1.0)
+
