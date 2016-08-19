c21f4e14822933f9089f7c444243e7d6fab7b9bc
Alexander Opremcak <opremcak@wisc.edu>
2016-08-19 14:38:47 -0600

    Added a checker at the end of dataCorruptionScript so that user does not need to visually inspect the file using the grapher.

--- dataChest/dataCorruptionTest.py
+++ dataChest/dataCorruptionTest.py
@@ -4,7 +4,8 @@ import time
 
 #create array of dataChest objects for data corruption tests
 dataChestObjArray = []
-for objIndex in range(0,100):
+numObjs = 100
+for objIndex in range(0,numObjs):
     dataChestObjArray.append(dataChest(["dataCorruptionTest"]))
     d = dataChestObjArray[objIndex]
     d.createDataset("AllOnes", [("indepName1", [1], "float64", "s")], [("depName1", [1], "float64", "V")])
@@ -12,10 +13,28 @@ for objIndex in range(0,100):
     d.addParameter("Y Label", "All Ones")
     d.addParameter("Plot Title", "Is Dataset Corrupted")
 
-
-for ii in range(0, 12*60*60): #will take about ~13 hours since it takes finite time to write to all 100 files
-    for objIndex in range(0,100):
+numWrites = 12*60*60 # vary if you like
+for ii in range(0, numWrites): #will take about ~13 hours since it takes finite time to write to all 100 files
+    for objIndex in range(0,numObjs):
         d = dataChestObjArray[objIndex]
         d.addData([[float(ii),1.0]])
     time.sleep(1.0)
+
+#Check for corrupted files
+
+for objIndex in range(0,numObjs):
+    d = dataChestObjArray[objIndex]
+    name = d.getDatasetName()
+    data = d.getData()
+    if len(data) != numWrites:
+        print "Invalid length for dataset with name=", name
+    else:
+        for ii in range(0, numWrites):
+            if list(data[ii]) != [float(ii), 1.0]:
+                print "Corrupted dataset with name =", name
+                print "row number ", ii, " has an incorrect value."
+                break #this way the number of prints = number of files with at least one corrupted row
+    
+    
+    
 
