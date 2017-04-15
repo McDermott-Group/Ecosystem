69121901c7fdb956f90cf4f85ffdc2d02fc97fa9
Chris Wilen <chriswilen@gmail.com>
2017-04-14 20:27:06 -0500

    labrad data chest.  *not tested.*  still need to implement createDataset I think?

--- dataChest/labradDataChest.py
+++ dataChest/labradDataChest.py
@@ -3,14 +3,22 @@ from labrad import units
 
 class labradDataChest(dataChest):
 
-    def createDataset(self):
-        pass
+    def createDataset(self, *args, **kwargs):
+        super(dataChest, self).createDataset(args, kwargs)
 
     def addData(self, data):
-        pass
+        indepVars, depVars = self.getVariables()
+        unitsList = [var[3] for var in indepVars + depVars]
+        for row in data:
+            row = [row[i][unitsList[i]] for i in range(len(row))]
+        super(dataChest, self).addData(data)
 
-    def getData(self, startIndex = np.nan, stopIndex = np.nan):
-        pass
+    def getData(self, *args, **kwargs):
+        super(dataChest, self).addData(*args, **kwargs)
+        indepVars, depVars = self.getVariables()
+        unitsList = [var[3] for var in indepVars + depVars]
+        for row in data:
+            row = [units.Value(row[i], unitsList[i]) for i in range(len(row))]
 
     def addParameter(self, paramName, paramValue, overwrite=False):
         try:
@@ -21,5 +29,9 @@ class labradDataChest(dataChest):
             value = paramValue
         super(dataChest, self).addParameter(paramName, value, paramUnits=units, overwrite)
 
-    def getParameter(self, *args, **kwargs):
-        resp = super(dataChest, self).getParameter(args, kwargs)
+    def getParameter(self, paramName, **kwargs):
+        resp = super(dataChest, self).getParameter(paramName, kwargs)
+        if self.getParameterUnits(paramName) is not None:
+            return units.Value(*resp)
+        else:
+            return resp
