c9dc360b4edfb64f33742df09f0351d5f9b57f56
Chris Wilen <pechenezhski@wisc.edu>
2017-04-17 19:02:30 -0500

    more bug fixes with unit conversions

--- dataChest/labradDataChest.py
+++ dataChest/labradDataChest.py
@@ -10,22 +10,28 @@ class labradDataChest(dataChest):
         indepVars, depVars = self.getVariables()
         unitsList = [var[3] for var in indepVars + depVars]
         for row in data:
-            row = [row[i][unitsList[i]] for i in range(len(row))]
+            for i in range(len(row)):
+                if unitsList[i] != '':
+                    try:
+                        row[i] = [x[unitsList[i]] for x in row[i]]
+                    except TypeError:
+                        row[i] = row[i][unitsList[i]]
         super(labradDataChest, self).addData(data)
 
     def getData(self, *args, **kwargs):
-        super(labradDataChest, self).addData(*args, **kwargs)
+        data = super(labradDataChest, self).getData(*args, **kwargs)
         indepVars, depVars = self.getVariables()
         unitsList = [var[3] for var in indepVars + depVars]
         for row in data:
             row = [units.Value(row[i], unitsList[i]) for i in range(len(row))]
+        return data
 
     def addParameter(self, paramName, paramValue, overwrite=False):
         try:
             units = paramValue.units
             value = paramValue[units]
         except AttributeError:
-            units = None
+            units = ''
             value = paramValue
         super(labradDataChest, self).addParameter(paramName, value, paramUnits=units, overwrite=overwrite)
 
