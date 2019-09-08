1ebbd0751b52cdfc743e7f2b5311fd3d458bed1e
ADR3\lab_user <labrad@physics.wisc.edu>
2019-09-08 13:05:21 -0500

    new datachest utils file

---
+++ dataChest/dataChestUtils.py
@@ -0,0 +1,41 @@
+import sys
+import os
+from PyQt4 import QtGui, QtCore
+import time
+import gc
+from dataChest import *
+
+
+class FilePicker(QtGui.QWidget):
+    """
+    An example file picker application
+    """
+    def __init__(self, parent = None):
+        self.app = QtGui.QApplication(sys.argv)
+        super(FilePicker, self).__init__(parent)
+        self.data_root = os.environ["DATA_ROOT"]
+        self.relativePath = None
+        self.selectedFileName = None
+        self.dialog = QtGui.QFileDialog()
+        self.dialog.fileSelected.connect(QtCore.QCoreApplication.instance().quit)
+        self.fullFilePath = self.dialog.getOpenFileName(self, 'Open File Up',self.data_root,"Text files (*.hdf5)")
+        self.fullFilePath = str(self.fullFilePath)
+        self.dialog.close()
+        self.getDatasetName()
+        
+    def getDatasetName(self):
+        if self.fullFilePath == '':
+            return
+        else:
+            if self.data_root not in self.fullFilePath.replace("/", "\\"):
+                print "The dataChest shall not venture outside of the DATA_ROOT path."
+                return
+            else:
+                self.relativePath = self.fullFilePath.strip(self.data_root).split("/")[3:-1]
+                self.selectedFileName = self.fullFilePath.strip(self.data_root).split("/")[-1]
+        return
+
+
+# if __name__ == "__main__":
+   # app = QtGui.QApplication(sys.argv)
+   # ex = FilePicker()
