b0413f119a72d1b21ce7c12f6285a902e3fdc96d
ADR3\lab_user <labrad@physics.wisc.edu>
2019-12-02 11:25:48 -0600

    Spectrum analyzer update. LIU

--- instruments/gpibdevices/Spectrum_Analyzer_PyVISA.py
+++ instruments/gpibdevices/Spectrum_Analyzer_PyVISA.py
@@ -2,33 +2,21 @@ import pyvisa
 import struct
 import time
 import numpy as np
-
+#
 rm = pyvisa.ResourceManager()
 sa = rm.open_resource('GPIB0::16::INSTR')
-
-# dp = sa.query('TDF P; TRA?;')
-# print(dp)
-
-# t1 = time.time()
-# dm = sa.query('TDF M; TRA?;')
-# dm_dBm = np.zeros(401)
-# t2 = time.time()
-# print(t2-t1)
-# print(dm)
-
-#
 sa.encoding = "latin-1"
+# sa.encoding = "ASCii"
 for i in range(10):
     print('i=', i)
     t1 = time.time()
     sa.write('TDF B; TRA?;')
     d = sa.read_raw()
-    # d = sa.query('TDF B; TRA?;')
     d_m = np.asarray(struct.unpack('>401H', d))
     d_m_dBm = np.zeros(401)
     for i in range(401):
         d_m_dBm[i]=d_m[i]/100.0-80.0
-    # print(d_m_dBm)
+    print(d_m_dBm)
     t2 = time.time()
     print(t2-t1)
 
